a476bd2bf8b00888e26aa5bcb0c0d5b91c839ac7
Aniketshinde1710 <aniketshinde1710@gmail.com>
2025-09-12 00:18:56 +0530

    Made some updates

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -19,7 +19,7 @@ export const profileData = {
 
 export const skillsData = {
   technical: [
-    "Jaava" ,"HTML", "CSS", "JavaScript", "React",
+    "Java" ,"HTML", "CSS", "JavaScript", "React",
     "MySQL", "Machine Learning", "N8N"
   ],
   tools: [
